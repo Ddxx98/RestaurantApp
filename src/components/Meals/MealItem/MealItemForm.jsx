8c063e3f4897f01ffc6eeaf68bfa3f2d7b90eb75
Dddxx98 <deexith2016@gmail.com>
2025-07-14 18:38:58 +0530

    commit 4

---
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -0,0 +1,13 @@
+import "./MealItemForm.css"
+import Input from "../../UI/Input"
+
+const MealItemForm = () => {
+  return (
+    <form className='form'>
+        <Input label="Amount" input={{type: "number", id: "amount", min: "1", max: "5", step: "1", defaultValue: "1"}} />
+        <button type="submit">+ Add</button>
+    </form>
+  )
+}
+
+export default MealItemForm
